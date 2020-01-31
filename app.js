826f6f60cfd6470e348a7f95b6a3b69ea431bd8f
julien <j.paput@gmail.com>
2020-01-31 16:30:09 +0100

    Remove Vue from project Add cors to dependency

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors');
 const app = express()
 
 var path = require('path');
 var router = require('./bin/router');
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', router);
 
